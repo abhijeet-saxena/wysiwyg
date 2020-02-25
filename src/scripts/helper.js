8ee94d7eec9b2fe377eb641d39f40a59494e2f96
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2020-02-26 00:00:00 +0530

    feat(app): Add PDF save logic

--- src/scripts/helper.js
+++ src/scripts/helper.js
@@ -86,44 +86,14 @@ export const copyToHTML = (target) => {
 
 // Sets the given text as active selection
 export const downloadPDF = (editor) => {
-  // var HTML_Width = 800
-  // var HTML_Height = 480
-  // var top_left_margin = 15
-  // var PDF_Width = HTML_Width + top_left_margin * 2
-  // var PDF_Height = PDF_Width * 1.5 + top_left_margin * 2
-  // var canvas_image_width = HTML_Width
-  // var canvas_image_height = HTML_Height
-
-  // var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1
-
   html2canvas(editor, {
-    width: window.innerWidth,
-    height: window.innerHeight,
-    windowWidth: window.innerWidth,
-    windowHeight: window.innerHeight,
+    scale: 1,
+    allowTaint: true,
+    useCORS: true,
   }).then(function (canvas) {
     var imgData = canvas.toDataURL('image/jpeg', 1.0)
-    // console.log(imgData)
-    // var pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height])
-    // pdf.addImage(
-    //   imgData,
-    //   'JPG',
-    //   top_left_margin,
-    //   top_left_margin,
-    //   canvas_image_width,
-    //   canvas_image_height
-    // )
-    // for (var i = 1; i <= totalPDFPages; i++) {
-    //   pdf.addPage(PDF_Width, PDF_Height)
-    //   pdf.addImage(
-    //     imgData,
-    //     'JPG',
-    //     top_left_margin,
-    //     -(PDF_Height * i) + top_left_margin * 4,
-    //     canvas_image_width,
-    //     canvas_image_height
-    //   )
-    // }
-    // pdf.save('Your_PDF_Name.pdf')
+    let pdf = new jsPDF('l', 'mm', 'a4')
+    pdf.addImage(imgData, 'PNG', 5, 5)
+    pdf.save('mypdf.pdf')
   })
 }
