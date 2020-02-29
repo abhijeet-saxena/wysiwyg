b2f35d9df90d452f8e319da40c0f3becf6ccd19b
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2020-03-01 00:00:00 +0530

    feat(app): Allow code blocks to be copied with styles

--- src/scripts/helper.js
+++ src/scripts/helper.js
@@ -67,6 +67,20 @@ export const insertImage = (event, selectedText) => {
 
 // Returns the current selection
 export const copyToHTML = (target) => {
+  if (document.querySelector('pre')) {
+    document.querySelector(
+      'pre'
+    ).style.cssText = ` background: rgba(30, 30, 30, 1);
+      color: white;
+      padding: 1rem;
+      margin: 5px 0rem;
+      border-radius: 5px;`
+
+    document.querySelector('pre').innerHTML = document
+      .querySelector('pre')
+      .innerHTML.trim()
+  }
+
   navigator.clipboard.writeText(target.innerHTML.trim())
 }
 
