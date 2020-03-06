802ca9e0aa1d2c1c87ae007a2b42b4f10ab074c4
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2020-03-07 00:00:00 +0530

    feat(app): Implement Heading functionality

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import {
   saveRange,
   alignContent,
   setColor,
+  formatBlock,
   createLink,
   insertImage,
   copyToHTML,
@@ -28,30 +29,40 @@ window.onload = () => {
   const imageForm = document.querySelector('.image-form')
   const colorForm = document.querySelector('.color-form')
   const highlightForm = document.querySelector('.highlight-color-form')
+  const headingForm = document.querySelector('.heading-form')
 
   const resetForms = () => {
     urlForm.classList.add('hide')
+    imageForm.classList.add('hide')
     colorForm.classList.add('hide')
     highlightForm.classList.add('hide')
+    headingForm.classList.add('hide')
   }
 
   // Set default styles
   document.execCommand('styleWithCSS', false, 'true')
-  document.execCommand('justifyLeft', null, null)
+  document.execCommand('justifyLeft', false, null)
+  document.execCommand('defaultParagraphSeparator', false, 'p')
   alignContent('align', 'justifyLeft')
+
+  document.body.addEventListener('click', (event) => {
+    if (!event.path.some((item) => item.tagName === 'MAIN')) resetForms()
+  })
 
   editor.addEventListener('click', (event) => {
     Object.keys(svgMap).forEach((svg) => {
+      resetForms()
       const button = document.querySelector(`[data-command="${svg}"]`)
       let isActive = false
 
-      if (svg !== 'createLink')
-        isActive = document.queryCommandState(svg) ? true : false
-      else {
+      if (svg === 'createLink')
         isActive = event.path.some((item) => item.tagName === 'A')
           ? true
           : false
+      else {
+        isActive = document.queryCommandState(svg) ? true : false
       }
+
       if (isActive) button.classList.add('active')
       else button.classList.remove('active')
     })
@@ -69,11 +80,6 @@ window.onload = () => {
     if (target.tagName !== 'BUTTON') return
     if (command !== 'createLink') resetForms()
 
-    if (!target.classList.contains('active')) {
-      target.style.animation = 'clicked 200ms ease-in-out'
-      target.onanimationend = () => (target.style.animation = '')
-    }
-
     if (type === 'align' || type === 'list') alignContent(type, command)
     else if (command === 'foreColor' || command === 'backColor') {
       selectedText = saveRange()
@@ -88,23 +94,39 @@ window.onload = () => {
         selectedText = saveRange()
         urlForm.classList.toggle('hide')
       }
+      document.querySelector('#url').focus()
       return
     } else if (command === 'insertImage') {
       selectedText = saveRange()
       imageForm.classList.toggle('hide')
+      document.querySelector('#image').focus()
+      return
+    } else if (command === 'heading') {
+      headingForm.classList.remove('hide')
       return
     } else if (command === 'copyHTML') {
       editor.focus()
-      document.execCommand('selectAll')
+      document.execCommand('selectAll', false, null)
       copyToHTML(editor)
+    } else if (command === 'codeBlock') {
+      formatBlock(event, '')
+      editor.innerHTML += `<p><br></p>`
+      editor.addEventListener('keydown', (e) => {
+        if (e.keyCode === 13) {
+          e.preventDefault()
+          document.execCommand('insertLineBreak')
+        }
+      })
+      return
     } else if (command === 'downloadPDF') downloadPDF(editor)
     else if (type !== 'once') target.classList.toggle('active')
 
-    document.execCommand(command, null, param)
+    document.execCommand(command, false, param)
   })
 
   urlForm.addEventListener('submit', (e) => createLink(e, selectedText))
   imageForm.addEventListener('submit', (e) => insertImage(e, selectedText))
   colorForm.addEventListener('click', (e) => setColor(e, 'foreColor'))
   highlightForm.addEventListener('click', (e) => setColor(e, 'backColor'))
+  headingForm.addEventListener('click', (e) => formatBlock(e, 'heading-form'))
 }
