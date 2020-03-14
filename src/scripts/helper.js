3802a6998f916c84289388ca27b3b91f4c89ff11
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2020-03-15 00:00:00 +0530

    feat(app): Handle list styles

--- src/scripts/helper.js
+++ src/scripts/helper.js
@@ -1,16 +1,25 @@
-export const alignContent = (command) => {
+// Handles all align operations
+export const alignContent = (type, command) => {
   document
-    .querySelectorAll('.align')
+    .querySelectorAll(`.${type}`)
     .forEach((button) => button.classList.remove('active'))
-  document.querySelector(`[data-command=${command}]`).classList.add('active')
+
+  if (type === 'align' || !document.queryCommandState(command))
+    document.querySelector(`[data-command=${command}]`).classList.add('active')
 }
 
+// export listElements = (command) =>{
+
+// }
+
+// Returns the current selection
 export const saveRange = () => {
   const selection = window.getSelection()
   if (selection.getRangeAt && selection.rangeCount)
     return selection.getRangeAt(0)
 }
 
+// Sets the given text as active selection
 export const getRange = (range) => {
   if (range) {
     const selection = window.getSelection()
