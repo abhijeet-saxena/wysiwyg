c1cfaf6bc69737e7fec5932a8ec599a1b33301b8
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2020-03-17 00:00:00 +0530

    feat(app): Allow customising link urls

--- src/main.js
+++ src/main.js
@@ -1,107 +1,81 @@
 // Imports
 import svgMap from './scripts/svg-map.js'
+import { alignContent, getRange, saveRange } from './scripts/helper.js'
 
 // Variables
+// DOM Selectors
 const editor = document.querySelector('.editor')
 const toolbar = document.querySelector('.toolbar')
-let range = null // Used to store the selected text
 
-const alignContent = (command) => {
-  document
-    .querySelectorAll('.align')
-    .forEach((button) => button.classList.remove('active'))
-  document.querySelector(`[data-command=${command}]`).classList.add('active')
-}
+// Used to store the selected text
+let selectedText = null
+let currentElement = null
 
 window.onload = () => {
   // Draws all toolbar buttons
   Object.keys(svgMap).forEach((svg) => (toolbar.innerHTML += svgMap[svg]))
 
+  const urlForm = document.querySelector('.url-form')
+
   // Set default styles
   document.execCommand('justifyLeft', null, null)
   alignContent('justifyLeft')
 
-  const urlForm = document.querySelector('.url-form')
-  const saveRange = () => {
-    const selection = window.getSelection()
-    if (selection.getRangeAt && selection.rangeCount)
-      range = selection.getRangeAt(0)
-  }
+  editor.addEventListener('click', (event) => {
+    Object.keys(svgMap).forEach((svg) => {
+      const button = document.querySelector(`[data-command="${svg}"]`)
+      let isActive = false
 
-  const getRange = () => {
-    if (range) {
-      const selection = window.getSelection()
-      selection.removeAllRanges()
-      selection.addRange(range)
-    }
-  }
-
-  editor.addEventListener(
-    'selectstart',
-    (event) => {
-      const button = document.querySelector(`[data-command="createLink"]`)
-
-      if (event.path.some((item) => item.tagName === 'A'))
-        button.classList.add('active')
-      else button.classList.remove('active')
-    },
-    false
-  )
-
-  editor.addEventListener(
-    'mouseup',
-    () => {
-      Object.keys(svgMap).forEach((svg) => {
-        if (svg !== 'createLink') {
-          const button = document.querySelector(`[data-command="${svg}"]`)
-          if (document.queryCommandState(svg)) button.classList.add('active')
-          else button.classList.remove('active')
-        }
-      })
-    },
-    false
-  )
-
-  toolbar.addEventListener(
-    'click',
-    (event) => {
-      const {
-        target,
-        target: {
-          dataset: { command, type },
-        },
-      } = event
-      let param = null
-
-      if (target.tagName !== 'BUTTON') return
-
-      if (!target.classList.contains('active')) {
-        target.style.animation = 'clicked 200ms ease-in-out'
-        target.onanimationend = () => (target.style.animation = '')
+      if (svg !== 'createLink')
+        isActive = document.queryCommandState(svg) ? true : false
+      else {
+        isActive = event.path.some((item) => item.tagName === 'A')
+          ? true
+          : false
       }
 
-      if (type === 'align') alignContent(command)
-      else if (command === 'createLink') {
-        saveRange()
-        urlForm.style.display = 'flex'
-        return
-      } else if (type !== 'once') target.classList.toggle('active')
+      if (isActive) button.classList.add('active')
+      else button.classList.remove('active')
+    })
+  })
 
-      document.execCommand(command, null, param)
-    },
-    false
-  )
+  toolbar.addEventListener('click', (event) => {
+    const {
+      target,
+      target: {
+        dataset: { command, type },
+      },
+    } = event
+    let param = null
 
-  urlForm.addEventListener(
-    'submit',
-    (event) => {
-      event.preventDefault()
-      const formData = new FormData(event.target)
-      var url = formData.get('url')
-      getRange()
-      urlForm.style.display = 'none'
+    if (target.tagName !== 'BUTTON') return
+
+    if (!target.classList.contains('active')) {
+      target.style.animation = 'clicked 200ms ease-in-out'
+      target.onanimationend = () => (target.style.animation = '')
+    }
+
+    if (type === 'align') alignContent(command)
+    else if (command === 'createLink') {
+      if (window.getSelection().toString()) {
+        selectedText = saveRange()
+        urlForm.classList.toggle('hide')
+      }
+      return
+    } else if (type !== 'once') target.classList.toggle('active')
+
+    document.execCommand(command, null, param)
+  })
+
+  urlForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const url = document.querySelector('#url').value.trim()
+    urlForm.classList.toggle('hide')
+
+    if (url) {
+      console.log(getRange(selectedText))
       document.execCommand('createLink', null, url)
-    },
-    false
-  )
+      // document.getSelection().anchorNode.parentElement.target = '_blank'
+    }
+  })
 }
