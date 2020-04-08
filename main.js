6ab8b8c60581d4447ac72f9211340d9f41a84804
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2020-04-09 02:54:51 +0530

    Updates

--- main.js
+++ main.js
@@ -3,12 +3,55 @@ import svgMap from "./svg-map.js";
 const editor = document.querySelector(".editor");
 const toolbar = document.querySelector(".toolbar");
 
-toolbar.addEventListener(
-  "click",
-  (event) => {
-    document.execCommand(event.target.dataset.func);
+const alignContent = (command) => {
+  document
+    .querySelectorAll(".align")
+    .forEach((button) => button.classList.remove("active"));
+
+  document.querySelector(`[data-command=${command}]`).classList.add("active");
+};
+
+editor.addEventListener(
+  "mouseup",
+  () => {
+    Object.keys(svgMap).forEach((svg) => {
+      const button = document.querySelector(`[data-command="${svg}"]`);
+      if (document.queryCommandState(svg)) button.classList.add("active");
+      else button.classList.remove("active");
+    });
   },
   false
 );
 
-Object.keys(svgMap).forEach((svg) => (toolbar.innerHTML += svgMap[svg]));
+toolbar.addEventListener(
+  "click",
+  (event) => {
+    const {
+      target,
+      target: {
+        dataset: { command, type },
+      },
+    } = event;
+    let param = null;
+
+    if (target.tagName !== "BUTTON") return;
+
+    if (!target.classList.contains("active")) {
+      target.style.animation = "clicked 200ms ease-in-out";
+      target.onanimationend = () => (target.style.animation = "");
+    }
+
+    if (type === "align") alignContent(command);
+    else if (type !== "once") target.classList.toggle("active");
+
+    document.execCommand(command, null, param);
+  },
+  false
+);
+
+window.onload = () => {
+  Object.keys(svgMap).forEach((svg) => (toolbar.innerHTML += svgMap[svg]));
+
+  document.execCommand("justifyLeft", null, null);
+  alignContent("justifyLeft");
+};
