049b5aa3654fbc864b7c49d9ae52e315d9edc40d
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2020-04-08 19:02:45 +0530

    Updates

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-alert(1);
+import svgMap from "./svg-map.js";
+
+const editor = document.querySelector(".editor");
+const toolbar = document.querySelector(".toolbar");
+
+toolbar.addEventListener(
+  "click",
+  (event) => {
+    document.execCommand(event.target.dataset.func);
+  },
+  false
+);
+
+Object.keys(svgMap).forEach((svg) => (toolbar.innerHTML += svgMap[svg]));
